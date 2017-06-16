ebcc99ae383b67976d6b80c84e16b7ad747580e2
Neo Liu <neo@zanadu.cn>
2017-06-16 13:34:29 +0800

    Clean code

--- snowflake.c
+++ snowflake.c
@@ -5,20 +5,6 @@
 
 #include "snowflake.h"
 
-#define START_TIMESTAMP 1497453478000
-
-#define SEQ_BIT 12
-#define MACHINE_BIT 5
-#define DATACENTER_BIT 5
-
-#define MAX_DATACENTER_NUM ((1 << DATACENTER_BIT) - 1)
-#define MAX_MACHINE_NUM ((1 << MACHINE_BIT) - 1)
-#define MAX_SEQ_NUM ((1 << SEQ_BIT) - 1)
-
-#define MACHINE_LEFT_OFFSET SEQ_BIT
-#define DATACENTER_LEFT_OFFSET (SEQ_BIT + MACHINE_BIT)
-#define TIMESTAMP_LEFT_OFFSET (DATACENTER_LEFT_OFFSET + DATACENTER_BIT)
-
 static uint64_t snowflake_next_timestamp();
 
 uint64_t last_timestamp = 0;
